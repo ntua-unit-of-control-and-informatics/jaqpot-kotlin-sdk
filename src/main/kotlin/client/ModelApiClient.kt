b9f9e896dc9767fee9a3163d8e69353db9f35008
alarv <alarvfm@gmail.com>
2024-11-14 10:46:01 +0200

    feat: expose model api to client

--- src/main/kotlin/client/ModelApiClient.kt
+++ src/main/kotlin/client/ModelApiClient.kt
@@ -6,7 +6,6 @@ import org.openapitools.client.api.DatasetApi
 import org.openapitools.client.api.ModelApi
 import org.openapitools.client.model.Dataset
 import org.openapitools.client.model.DatasetType
-import retrofit2.Call
 
 
 class ModelApiClient(
@@ -14,21 +13,12 @@ class ModelApiClient(
     apiSecret: String
 ) : BaseApiClient(apiKey, apiSecret) {
 
-    private val modelApi: ModelApi = retrofit.create(ModelApi::class.java)
+    val modelApi: ModelApi = retrofit.create(ModelApi::class.java)
+        get() = field
     private val datasetApi: DatasetApi = retrofit.create(DatasetApi::class.java)
 
     companion object {
         const val DATASET_CHECK_INTERVAL: Long = 2000
-    }
-
-    fun predictAsync(modelId: Long, input: List<Any>): Call<Void>? {
-        return modelApi.predictWithModel(
-            modelId,
-            Dataset.Builder().type(DatasetType.PREDICTION)
-                .entryType(Dataset.EntryTypeEnum.ARRAY)
-                .input(input)
-                .build()
-        )
     }
 
     fun predictSync(modelId: Long, input: List<Any>): Dataset {
@@ -41,7 +31,7 @@ class ModelApiClient(
         ).execute()
 
         if (!response.isSuccessful) {
-            val message = response.errorBody()?.string();
+            val message = response.errorBody()?.string()
             if (response.code() == 401) {
                 throw JaqpotSDKException("Prediction failed: Unauthenticated \n$message", response.errorBody())
             } else if (response.code() == 403) {
